5a5f700f75ec23e95f0089ab8ec91d3e13179a6c
Alvinosaur <ashek@andrew.cmu.edu>
2018-07-07 12:22:27 -0700

    Working user controls, bug where communication lost for ~5-10 seconds

--- user_controls/user_controls.ino
+++ user_controls/user_controls.ino
@@ -1,37 +1,40 @@
-/*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
+#include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
+#define CONTROLX analogRead(A0);
+#define CONTROLY analogRead(A1);
 RF24 radio(7, 8); // CE, CSN
-const byte addresses[][6] = {"00001", "00002"};
-boolean buttonState = 0;
+const byte address[][6] = {"00001","00002"};
+
 void setup() {
-  uint_8 potValues[2];
   radio.begin();
-  radio.openWritingPipe(addresses[1]); // 00001
-  radio.openReadingPipe(1, addresses[0]); // 00002
-  radio.setPALevel(RF24_PA_HIGH); //RF24_PA_HIGH, ..MAX, 
+  radio.openWritingPipe(address[0]);
+  radio.openReadingPipe(0, address[1]);
+  radio.setPALevel(RF24_PA_HIGH);
   Serial.begin(9600);
 }
 void loop() {
-  radio.startListening();
-  if (radio.available())
-  {
-    radio.read(&data, sizeof(data));
-    Serial.println(data);
-  }
   radio.stopListening();
-  }
-  potValues[0] = analogRead(A0)
-  potValues[1] = analogRead(A1);
-  radio.write(&potValues, sizeof(potValues));
-  delay(500);
+  sendControls();
+  radio.startListening();
+  readData();
+  delay(1000);
 }
 
+void sendControls(){
+  uint8_t x_val = CONTROLX;
+  uint8_t y_val = CONTROLY;
+  uint8_t text[] = {x_val,y_val};
+  radio.write(&text, sizeof(text));
+}
+
+void readData(){
+  uint8_t data[2];
+  radio.read(&data, sizeof(data));
+  for (uint8_t i=0; i< sizeof(data); i++)
+  {
+    Serial.print(data[i]); Serial.print(',');
+  }
+  Serial.println();
+}
